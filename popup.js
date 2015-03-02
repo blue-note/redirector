f5e41193ce0a1ecf230f1b74d362cb3932335508
Janita Chalam <janitachalam@vlan417-236.subnet-232.amherst.edu>
2015-03-01 20:26:46 -0500

    add jquery in inputs

--- popup.js
+++ popup.js
@@ -1,17 +1,26 @@
-paused = false;
+var paused = false;
 window.onload = function() {
-paused = false;
-document.getElementById("pause").onclick=
-function() {
+
+$("#submit").click(function() {
+console.log("hey");
+
+var inputs = $("#urlForm").serialize();
+
+});
+
+
+/*
+document.getElementById("pause").onclick = function() {
 paused = !paused;
-document.getElementById("PAUSE").innerHTML = paused;
+if (paused == true) document.getElementById("pause").innerHTML = "unpause";
+else document.getElementById("pause").innerHTML = "pause";
+window.close();
+
+}
+*/
+
 
 }
 
-}
 
-function isPaused() {return paused;}
 
-function beans() {
-	return "beans";
-}
