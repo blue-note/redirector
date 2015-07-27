b21843dcb7806bb5dc2f3d070d551f3f01f04512
Janita Chalam <janitachalam@172-16-112-83.dynapool.nyu.edu>
2015-07-26 22:33:08 -0400

    add popup-background communication

--- background.js
+++ background.js
@@ -1,8 +1,22 @@
-function onBeforeRequestHandler(details) {
+var paused;
+var onBeforeRequestHandler = function(details) {
+	var result;
 	var url = details.url;
-	var fb_one = url.search("facebook");
-	var fb_two = url.search("fb");
-	if (fb_one != -1 || fb_two != -1) return {"redirectUrl": "http://www.feedly.com"};
-}
+	var fb_one = url.search("facebook.com");
+	var fb_two = url.search("fb.com");
+	if (!paused && (fb_one != -1 || fb_two != -1)) {
+		return {"redirectUrl": "http://www.feedly.com/"};
+	}
+};
 
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestHandler, {urls: ["http://*/*", "https://*/*"]}, ["blocking"]);
+chrome.extension.onConnect.addListener(function(port) {
+	port.onMessage.addListener(function(msg) {
+		console.log('message received' + msg);
+		paused = msg;
+	});
+});
+
+
+chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestHandler, {
+	urls: ["http://*/*", "https://*/*"]
+}, ["blocking"]);
--- popup.js
+++ popup.js
@@ -1,12 +1,27 @@
-var paused = false;
 $(document).ready(function() {
+	var port = chrome.extension.connect({"name": "sync"});
+	var paused;
+	chrome.storage.sync.get("paused", function(object) {
+	paused = object.paused;
+	setText();
+	});
+
 	$(".url-form").submit(function(event) {
 		event.preventDefault();
 		var $inputs = $("input").serializeArray();
-		console.log($inputs);
 	});
 
 	$("#pause").click(function() {
 		paused = !paused;
+		setText();
+		port.postMessage(paused);	
+		chrome.storage.sync.set({"paused": paused});
 	});
+
+	var setText = function() {
+		var text;
+		if (paused) text = "unpause";
+		else text = "pause";
+		$("#pause").text(text);
+	};
 });
